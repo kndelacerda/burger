412e86679f39b2083109735895816845dfc6ff85
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-11 23:40:02 -0500

    Added db config / sequelize

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express = require("express");
+var db = require("./models");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("Listening on port %s", PORT);
+    });
+});
