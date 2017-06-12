15b7fc35aaff40119f0be8975cf74a9946ca55de
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-12 00:42:32 -0500

    Test run

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var db = require("./models");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var PORT = process.env.PORT || 3000;
+var routes = require('./controllers/burgers_controllers.js');
 var app = express();
 
 db.sequelize.sync().then(function() {
@@ -10,3 +11,18 @@ db.sequelize.sync().then(function() {
         console.log("Listening on port %s", PORT);
     });
 });
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.use('/', routes);
