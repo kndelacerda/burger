7cd7ddbe1cdc36a71a5820510318dc5d0e33854f
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-18 20:49:00 -0500

    Added error catcher

--- server.js
+++ server.js
@@ -32,31 +32,31 @@ app.use(methodOverride('_method'));
 var controller = require("./controllers/burgers_controllers");
 app.use('/', controller);
 
-// // Catch errors and forward them to the below error handlers
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
+// Catch errors and forward them to the below error handlers
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-// // Error handler - development error handler will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
+// Error handler - development error handler will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-// // Error handler - production handler not leaking stacktrace to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
+// Error handler - production handler not leaking stacktrace to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 module.exports = app;
