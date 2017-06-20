a21d64e42e460df5e02baca1743c0f3f5fe05635
Kristen De La Cerda <kndelacerda@gmail.com>
2017-06-20 12:39:05 -0500

    Revert

--- config/connection.js
+++ config/connection.js
@@ -1,48 +1,48 @@
-// // *********************************************************************************
-// // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// // *********************************************************************************
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
 
-// // Require mysql
-// var mysql = require("mysql");
+// Require mysql
+var mysql = require("mysql");
 
-// /**
-//  * If JAWSDB_URL is available (Heroku), use it.
-//  * Otherwise use local mysql configuration.
-//  */
-// if (process.env.JAWSDB_URL)
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// else {
-//     var keys = require('./keys');
+/**
+ * If JAWSDB_URL is available (Heroku), use it.
+ * Otherwise use local mysql configuration.
+ */
+if (process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+else {
+    var keys = require('./keys');
 
-//     connection = mysql.createConnection({
-//         host: keys.db.host,
-//         user: keys.db.user,
-//         password: keys.db.password,
-//         database: keys.db.database
-//     });
-// }
+    connection = mysql.createConnection({
+        host: keys.db.host,
+        user: keys.db.user,
+        password: keys.db.password,
+        database: keys.db.database
+    });
+}
 
-// // Connects to the DB
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error('Connection error: ' + err.stack);
-//         return;
-//     }
-//     console.log('Connection threadId: ' + connection.threadId);
-// });
-
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
+// Connects to the DB
+connection.connect(function(err) {
+    if (err) {
+        console.error('Connection error: ' + err.stack);
+        return;
+    }
+    console.log('Connection threadId: ' + connection.threadId);
 });
 
-connection.end();
+// var mysql = require('mysql');
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//     if (err) throw err;
+
+//     console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection.end();
 
 module.exports = connection;
 
